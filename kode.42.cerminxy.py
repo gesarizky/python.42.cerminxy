786162ad43fcc0e56e6a242ce51c67b9e1026c3f
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-10-26 15:35:04 +0700

    Add files via upload

---
+++ kode.42.cerminxy.py
@@ -0,0 +1,22 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+x = float(input("Tuliskan Nilai X : "))
+y = float(input("Tuliskan Nilai Y: "))
+
+# Mengkonversi
+x1 = - x
+y1 = - y 
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Hasil Pencerminan A(%d,%d)' %(x,y))
+print('Pada Sumbu X ' )
+print('Maka X* = %d'  %(x1))
+print('Maka Y* = %d'  %(y))
+print('Pada Sumbu Y ' )
+print('Maka X* = %d'  %(x))
+print('Maka Y* = %d'  %(y1))
+print('==========================================================')
